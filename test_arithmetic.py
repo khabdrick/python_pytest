dc529186acf0795efafba99db8e106bd9259b665
Muhammed Ali <muhamzyali@gmail.com>
2021-05-21 13:01:13 +0100

    travis ci

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -5,7 +5,9 @@ import arithmetic
 def test_sum():
     assert arithmetic.sum(5, 2)== 7
     assert arithmetic.sum(100, 200)==300
+    assert type(arithmetic.sum("Hi ", "there")) is str 
+    assert arithmetic.sum("Hi ", "there") == "Hi there"
 
-def test_sub():
-    assert arithmetic.minus(5, 2)==3
+def test_minus():
+    assert arithmetic.minus(5, 2) >= 2
     assert arithmetic.minus(-5, -5)==0
